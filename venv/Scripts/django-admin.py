37e3faffeae3b793979bab8a4345f4471c80ac7c
Vkash Poudel <31735302+bikashpoudel4@users.noreply.github.com>
2022-06-13 19:06:31 +0200

    Delete venv directory

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!G:\PROJECTS-Programming\Python\PyCharmProjects\portfolio\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
